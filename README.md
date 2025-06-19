c582fb6c060693a7cd8fcd4a4082c303d85a73d2
Rubnawaz Qureshi <m.rubnawazqureshi@gmail.com>
2025-06-19 08:27:02 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# install typescript
+npm install -g typescript
+
+# check version
+tsc --version
+
+# initialize typescript
+tsc --iniit
+
+# set working path in tsconfig.json
+rootDir "./src",
+outDir "./dist"
+
+
+# compile the code
+tsc 
---
+++ src/index.ts
@@ -0,0 +1,130 @@
+// Basic Types
+let id: number = 5
+let company: string = 'Amazon'
+let isPublished: boolean = true
+let x: any = 'Hello'
+
+let ids: number[] = [1, 2, 3, 4, 5]
+let arr: any[] = [1, true, 'Hello']
+
+// Tuple
+let person: [number, string, boolean] = [1, 'Rabbi', true]
+
+// Tuple Array
+let employee: [number, string][]
+
+employee = [
+  [1, 'Ali'],
+  [2, 'Raza'],
+  [3, 'Rabbi'],
+]
+
+// Union
+let pid: string | number
+pid = '22'
+
+// Enum
+enum Direction1 {
+  Up = 1,
+  Down,
+  Left,
+  Right,
+}
+
+enum Direction2 {
+  Up = 'Up',
+  Down = 'Down',
+  Left = 'Left',
+  Right = 'Right',
+}
+
+// Objects
+type User = {
+  id: number
+  name: string
+}
+
+const user: User = {
+  id: 1,
+  name: 'John',
+}
+
+// Type Assertion
+let cid: any = 1
+// let customerId = <number>cid
+let customerId = cid as number
+
+// Functions
+function addNum(x: number, y: number): number {
+  return x + y
+}
+
+// Void
+function log(message: string | number): void {
+  console.log(message)
+}
+
+// Interfaces
+interface UserInterface {
+  readonly id: number
+  name: string
+  age?: number
+}
+
+const user1: UserInterface = {
+  id: 1,
+  name: 'John',
+}
+
+interface MathFunc {
+  (x: number, y: number): number
+}
+
+const add: MathFunc = (x: number, y: number): number => x + y
+const sub: MathFunc = (x: number, y: number): number => x - y
+
+interface PersonInterface {
+  id: number
+  name: string
+  register(): string
+}
+
+// Classes
+class Person implements PersonInterface {
+  id: number
+  name: string
+
+  constructor(id: number, name: string) {
+    this.id = id
+    this.name = name
+  }
+
+  register() {
+    return `${this.name} is now registered`
+  }
+}
+
+const rubnawaz = new Person(1, 'Rubnawaz Qureshi')
+const ali = new Person(2, 'Ali Raza')
+
+// Subclasses
+class Employee extends Person {
+  position: string
+
+  constructor(id: number, name: string, position: string) {
+    super(id, name)
+    this.position = position
+  }
+}
+
+const emp = new Employee(3, 'Hassaan', 'Developer')
+
+// Generics
+function getArray<T>(items: T[]): T[] {
+  return new Array().concat(items)
+}
+
+let numArray = getArray<number>([1, 2, 3, 4])
+let strArray = getArray<string>(['ali', 'nouman', 'rabbi'])
+
+strArray.push('farhan') 
